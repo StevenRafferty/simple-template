76f8cceee2968f630b09b7362cbf510402652a76
Steven Rafferty <stevenrafferty2@protonmail.com>
2020-06-18 21:03:54 +0100

    Added handlebars-helpers for extra functionality, added bootstrap, created basic user logic

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 // Packages
 const express = require('express');
+// docs: https://handlebarsjs.com/
 const exphbs = require('express-handlebars');
 
 // Init express
 const app = express();
 
+// Init handlebars helper (https://www.npmjs.com/package/handlebars-helpers)
+require('handlebars-helpers')();
+
 // Set view engine
-app.use('handlebars', exphbs());
-app.use('view engine', 'handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Set assets
+app.use('/assets', express.static('assets'));
+
+const getUsers = async () => {
+  return [
+    {
+      name: 'Steve',
+      age: 22
+    },
+    {
+      name: 'Ben',
+      age: 24
+    },
+    {
+      name: 'Jacob',
+      age: 23
+    }
+  ];
+} 
+
+app.get('/', async (req, res) => {
+  res.render('home', { users: await getUsers() });
+});
 
 // Start server
 app.listen(8080, () => console.log('Server started!!!'));
